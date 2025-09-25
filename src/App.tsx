786a09d2b05531d801505d83fb7b4c5cf8b46204
michael-antar <michaeldantar@gmail.com>
2025-09-25 07:42:00 -0700

    Update layout of cards

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,20 @@ import { projectsData } from './data/projects';
 function App() {
     return (
         <Layout>
-            <HeroSection />
-            <EducationSection />
-            <ExperienceSection experiences={experienceData} />
-            <ProjectsSection projects={projectsData} />
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+                <div className="lg:col-span-1 flex flex-col gap-8">
+                    <HeroSection />
+                    <EducationSection />
+                </div>
+
+                <div className="lg:col-span-1">
+                    <ExperienceSection experiences={experienceData} />
+                </div>
+
+                <div className="lg:col-span-3">
+                    <ProjectsSection projects={projectsData} />
+                </div>
+            </div>
         </Layout>
     );
 }
