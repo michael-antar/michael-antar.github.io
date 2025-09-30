009835b42db64d461a7e68f8eb19029a7285e26d
michael-antar <michaeldantar@gmail.com>
2025-09-29 23:14:48 -0700

    Attach link to project description

--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import orderlyVideoLight from '@/assets/orderly-demo-light.mp4';
 import orderlyVideoDark from '@/assets/orderly-demo-dark.mp4';
 import orderlyThumbnailLight from '@/assets/orderly-thumb-light.png';
@@ -6,7 +7,7 @@ import orderlyThumbnailDark from '@/assets/orderly-thumb-dark.png';
 export interface Project {
     title: string;
     pitch: string;
-    description: string;
+    description: React.ReactNode;
     features: string[];
     techStack: string[];
     links: {
@@ -23,8 +24,22 @@ export const projectsData: Project[] = [
     {
         title: 'Orderly - Elo Rating App',
         pitch: 'A web app for logging and ranking your experiences.',
-        description:
-            'I built this app to solve the problem of ranking items in a list subjectively. Using the Elo rating system, users can easily compare two items at a time to generate a dynamically updated, ranked list.',
+        description: (
+            <>
+                I built this app to solve the problem of ranking items in a list
+                subjectively. Using the{' '}
+                <a
+                    href="https://en.wikipedia.org/wiki/Elo_rating_system"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="font-semibold text-blue-600 dark:text-blue-400 visited:text-purple-600 dark:visited:text-purple-400 hover:underline"
+                >
+                    Elo rating system
+                </a>
+                , users can easily compare two items at a time to generate a
+                dynamically updated, ranked list.
+            </>
+        ),
         features: [
             'Dynamic Elo rating calculation',
             'User authentication and data persistence',
